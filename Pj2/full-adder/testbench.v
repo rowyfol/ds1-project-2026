0a86a248955ae0b9df83d02db6827fdcaa0f9ccb
Ashkan Namjoo <ashkan.namjoo@eng.uk.ac.ir>
2026-06-19 21:22:20 +0330

    Update testbench.v

--- Pj2/full-adder/testbench.v
+++ Pj2/full-adder/testbench.v
@@ -1,23 +1,23 @@
 module tb_full_adder;
-reg A, B, Cin;
-wire S, Co;
-full_adder DUT(
-.A(A),
-.B(B),
-.Cin(Cin),
-.S(S),
-.Co(Co)
-);
-initial begin
-A=0; B=0; Cin=0;
-#20;
+  reg A, B, Cin;
+  wire S, Co;
+  full_adder DUT(
+    .A(A),
+    .B(B),
+    .Cin(Cin),
+    .S(S),
+  .Co(Co)
+  );
+  initial begin
+    A=0; B=0; Cin=0;
+    #20;
 
-A=0; B=1; Cin=1;
-#20;
+    A=0; B=1; Cin=1;
+    #20;
 
-A=1; B=1; Cin=1;
-#20;
+    A=1; B=1; Cin=1;
+    #20;
 
-$stop;
-end
+    $stop;
+  end
 endmodule
