5e33411f9d9dee0d60c7332a1325410f4f9ba4ef
Ashkan Namjoo <ashkan.namjoo@eng.uk.ac.ir>
2026-06-19 21:23:56 +0330

    Update testbench.v

--- Pj2/2-1-mux/testbench.v
+++ Pj2/2-1-mux/testbench.v
@@ -1,22 +1,22 @@
 module testbench;
-reg I0,I1,Sel;
-wire Y;
-mux_main DUT(
-    I0,I1,Sel,Y
-);
+    reg I0,I1,Sel;
+    wire Y;
+    mux_main DUT(
+        I0,I1,Sel,Y
+    );
 
 
-initial begin
+    initial begin
 
-I0=0; I1=1; Sel=0;
-#20;
+        I0=0; I1=1; Sel=0;
+        #20;
 
-I0=0; I1=1; Sel=1;
-#20;
+        I0=0; I1=1; Sel=1;
+        #20;
 
-I0=1; I1=0; Sel=1;
-#20;
+        I0=1; I1=0; Sel=1;
+        #20;
 
-$stop;
-end
+        $stop;
+    end
 endmodule
