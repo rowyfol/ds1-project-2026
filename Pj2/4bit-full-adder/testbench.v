3259eb403884a742007a6cd1e9e7c7e27a4cfc7d
Ashkan Namjoo <ashkan.namjoo@eng.uk.ac.ir>
2026-06-19 21:46:23 +0330

    Make Project 2 code more class-style

--- Pj2/4bit-full-adder/testbench.v
+++ Pj2/4bit-full-adder/testbench.v
@@ -1,28 +1,30 @@
+`timescale 1ns/1ps
+
 module tb_adder4;
   reg [3:0] A, B;
   reg Cin;
   wire [3:0] S;
   wire Cout;
-  adder4 DUT(
-      A, B, Cin, S, Cout
-  );
+
+  adder4 DUT(A, B, Cin, S, Cout);
+
   initial begin
+    $dumpfile("Pj2/4bit-full-adder/adder4.vcd");
+    $dumpvars(0, tb_adder4);
+    $monitor("time=%0t A=%b B=%b Cin=%b  S=%b Cout=%b", $time, A, B, Cin, S, Cout);
 
-    A=4'b0011;
-    B=4'b0101;
-    Cin=0;
-    #50;
+    A = 4'b0011; B = 4'b0101; Cin = 0;
+    #80;
 
-    A=4'b1111;
-    B=4'b0001;
-    Cin=0;
-    #50;
+    A = 4'b1111; B = 4'b0001; Cin = 0;
+    #80;
 
-    A=4'b1010;
-    B=4'b0110;
-    Cin=1;
-    #50;
+    A = 4'b1010; B = 4'b0110; Cin = 1;
+    #80;
 
-    $stop;
+    A = 4'b1111; B = 4'b1111; Cin = 1;
+    #80;
+
+    $finish;
   end
 endmodule
---
+++ Pj2/8bit-adders/testbench.v
@@ -0,0 +1,35 @@
+`timescale 1ns/1ps
+
+module tb_adders8;
+  reg [7:0] A, B;
+  reg Cin;
+  wire [7:0] S_ripple, S_select;
+  wire Cout_ripple, Cout_select;
+
+  ripple_carry_adder8 RCA(A, B, Cin, S_ripple, Cout_ripple);
+  carry_select_adder8 CSA(A, B, Cin, S_select, Cout_select);
+
+  initial begin
+    $dumpfile("Pj2/8bit-adders/adders8.vcd");
+    $dumpvars(0, tb_adders8);
+    $monitor("time=%0t A=%h B=%h Cin=%b  ripple=%b_%h  select=%b_%h",
+             $time, A, B, Cin, Cout_ripple, S_ripple, Cout_select, S_select);
+
+    A = 8'h00; B = 8'h00; Cin = 0;
+    #120;
+
+    A = 8'h0f; B = 8'h01; Cin = 0;
+    #120;
+
+    A = 8'h55; B = 8'haa; Cin = 1;
+    #120;
+
+    A = 8'hff; B = 8'h01; Cin = 0;
+    #120;
+
+    A = 8'hff; B = 8'hff; Cin = 1;
+    #120;
+
+    $finish;
+  end
+endmodule
